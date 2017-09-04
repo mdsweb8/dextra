b68a28265c54ce612db6d284e07519ace086d526
mdsweb8 <31604373+mdsweb8@users.noreply.github.com>
2017-09-03 23:12:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include ("includes/head.php"); ?>
+</head>
+<body>    
+<h1 class="none">Bem Vindo a Dextra</h1>
+
+<header class="full">           
+    <?php include ("includes/menu.php"); ?>
+    <div class="esconder-desktop">oi</div>
+    <div class="esconder">
+        <div class="fullscreen">
+            <video loop muted autoplay class="video">
+                <source src="http://dextra.com.br/wp-content/themes/dextra-2015/images/video-highlight.webm" type="video/webm">
+                <source src="http://dextra.com.br/wp-content/themes/dextra-2015/images/video-highlight.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="home-content">
+            <div class="col-md-8 col-md-offset-2">
+            <center>
+                <span class="txt-rotate" data-period="2000" data-rotate='[ 
+                    "ORIENTANDO A JORNADA DE SUA TRANSFORMAÇÃO DIGITAL.",
+                    "DESENVOLVEMOS SOFTWARE SOB MEDIDA PARA NEGÓCIOS DIGITAIS."]'>
+                </span>
+            </center>
+            </div>
+        </div>
+    </div>
+</header> 
+    
+<section id="dextra">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding">
+                <h1>De<p class="x">x</p>tra. </h1>
+                <span>A MELHOR EMPRESA. AS MELHORES IDEIAS. OS MELHORES RESULTADOS.</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 wow fadeIn">
+                <article>
+                    <img alt="Desenvolvimento Inteligente" title="Desenvolvimento Inteligente" border="0" src="img/icons/desenvolvimento-inteligente.png" />
+                    <h2>INTELIGÊNCIA</h2>
+                    <p>Trazemos novas ideias e perspectivas. Estamos atentos as mudanças no mercado e aplicamos conceitos de UX para criar produtos digitais inovadores.</p>
+                </article>
+            </div>
+            <div class="col-md-3 wow fadeIn" data-wow-delay=".2s">
+                <article>
+                    <img alt="Experiência" title="Experiência" border="0" src="img/icons/experiencia.png" />
+                    <h2>EXPERIÊNCIA</h2>
+                    <p>Mais de um milhão de horas em projetos ágeis de sucesso, centenas de produtos digitais entregues nos últimos 14 anos</p>
+                </article>
+            </div>
+            <div class="col-md-3 wow fadeIn" data-wow-delay=".3s">
+                <article>
+                    <img alt="Criatividade" title="Criatividade" border="0" src="img/icons/criatividade.png" />
+                    <h2>CRIATIVIDADE</h2>
+                    <p>Somos considerados um dos melhores lugares para se trabalhar, atraindo jovens talentos e profissionais experientes, que se desafiam todos os dias a criar as melhores soluções para nossos clientes.</p>
+                </article>
+            </div>
+            <div class="col-md-3 wow fadeIn" data-wow-delay=".4s">
+                <article>
+                    <img alt="Qualidade" title="Qualidade" border="0" src="img/icons/qualidade.png" />
+                    <h2>QUALIDADE</h2>
+                    <p>Considerada do começo ao fim do processo de desenvolvimento, para garantir entregas de produtos digitais de alto nível.</p>
+                </article>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <a href="#cases"><button>Confira os Cases de Sucesso</button></a>
+            </div>
+        </div>  
+    </div>
+</section> 
+    
+<section id="milestones" class="esconder">
+    <div class="container">
+        <h1 class="none">Nossas Conquistas</h1>
+        <div class="col-md-3">
+            <center><span class="counter">1000000</span>
+            <p>Horas de Desenvolvimento</p></center>
+        </div>
+        <div class="col-md-3">
+            <span class="counter">10</span>
+            <p>Anos de Projetos Ágeis</p>
+        </div>
+        <div class="col-md-3">
+            <span class="counter">1000000</span>
+            <p>Xícaras de Café</p>
+        </div>
+        <div class="col-md-3">
+            <span class="counter">1000</span>
+            <p>Projetos Entregues</p>
+        </div>
+    </div>
+</section>
+    
+<section id="solucoes">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding">
+                <center><h1 class="wow fadeIn">Soluç<p class="o">õ</p>es. </h1>
+                <span class="wow bounceIn">ILUMINANDO A JORNADA DE SUA TRANSFORMAÇÃO DIGITAL.</span></center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-xs-12 wow bounceInLeft">
+                <h2>APLICAÇÕES MOBILE</h2>
+                <p>Criamos experiências digitais em plataformas móveis. No desafio da transformação digital, não basta o desenvolvimento de uma App, é necessário a criação de experiências digitais ricas sobre plataformas móveis, que se integrem à estratégia da empresa, melhorem a experiência do usuário (UX) e alavanquem os resultados do negócio.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-1 col-xs-12 wow bounceInLeft" data-wow-delay=".2s">
+                <h2>APLICAÇÕES WEB</h2>
+                <p>Na jornada para a digitalização dos negócios, Aplicações Web continuam sendo um dos principais canais para alcançar mercados e atender os clientes de forma simples e direta. Nós desenvolvemos sob-medida aplicações Web centradas na experiência do usuário (UX) e nos resultados de negócio, com engenharia e tecnologia para torná-las sólidas e escaláveis, e também implantamos e sustentamos aplicações desenvolvidas.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 col-xs-12 wow bounceInLeft" data-wow-delay=".3s">
+                <h2>PLATAFORMAS E APIS</h2>
+                <p>Na sua jornada para a transformação digital, é essencial que as empresas utilizem todo o potencial de seus ativos de software existentes internamente. Para isto, é necessário que estes ativos sejam preparados para se integrarem com as novas soluções digitais.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-3 col-xs-12 wow bounceInLeft" data-wow-delay=".4s">
+                <h2>INOVAÇÃO DIGITAL</h2>
+                <p>NA digitalização dos negócios traz oportunidades que necessitam de uma nova forma de atuação, com times de negócio e tecnologia atuando de forma integrada para a criação da inovação digital necessária para esta nova dinâmica de negócios.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-3 col-xs-12 wow bounceInLeft" data-wow-delay=".5s">
+                <h2>CLOUD COMPUTING</h2>
+                <p>Cloud Computing, mais do que uma inovação tecnológica, é uma plataforma para viabilização de novas oportunidades de negócio! Desenvolvemos software utilizando plataformas de Cloud Computing desde de 2009. Crescemos junto com a utilização de nuvem, adquirindo grande know-how das ferramentas disponíveis.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 col-xs-12 wow bounceInLeft" data-wow-delay=".6s">
+                <h2>UX E DESIGN THINKING DIGITAL</h2>
+                <p>Através de metodologias de pesquisa e design thinking, estudamos e entendemos as pessoas que vão utilizar o software, unindo a visão de negócio com a visão do usuário e a tecnologia necessária.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-1 col-xs-12 wow bounceInLeft" data-wow-delay=".7s">
+                <h2>MACHINE LEARNING</h2>
+                <p>Desenvolvemos soluções inteligentes que permitem analisar e compreender grandes volumes de dados, através de um processo rápido, integrado e efetivo. Estas soluções se baseiam em técnicas de inteligência artificial e machine learning que permitem identificar padrões que podem impulsionar seus produtos e serviços.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-xs-12 wow bounceInLeft" data-wow-delay=".8s">
+                <h2>SERVIÇOS EM BANCO DE DADOS</h2>
+                <p>PostgreSQL – A Dextra é reconhecida internacionalmente pela sua experiência na prestação de serviços em PostgreSQL. Desde 1999 atuando em grandes projetos, suporte e consultorias, contamos com uma equipe de especialistas que são referência, oferecendo serviços de qualidade para algumas das maiores e mais importantes implementações de PostgreSQL no Brasil.</p>
+            </div>
+        </div>   
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <a href="#contato"><button>Entre em Contato</button></a>
+            </div>
+        </div>   
+    </div>
+</section>    
+
+
+<section id="cases">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding">
+                <center><h1>C<p class="x">a</p>ses.</h1>
+                <span>EMPRESAS QUE CONFIAM NO NOSSO TRABALHO.</span></center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 wow bounceIn">
+                <article>
+                    <h1 class="none">Nextel</h1>
+                    <img alt="" title="" border="0" src="img/nextel.png" />
+                    <p>Dextra é a parceira da Nextel para sua estratégia disruptiva de colocar o cliente no controle de sua conta de telefonia móvel.</p>
+                </article>
+            </div>
+            <div class="col-md-4 wow bounceIn" data-wow-delay=".2s">
+                <article>
+                    <h1 class="none">99 Taxis</h1>
+                    <img alt="" title="" border="0" src="img/99taxis.png" />
+                    <p>Expertise em aceleração digital aumenta número de corridas e market share da 99Taxis.</p>
+                </article>
+            </div>
+            <div class="col-md-4 wow bounceIn" data-wow-delay=".3s">
+                <article>
+                    <h1 class="none">L'OREAL</h1>
+                    <img alt="" title="" border="0" src="img/loreal.png" />
+                    <p>Desenvolvemos o app mobile voltado para força de vendas da L’Oreal Brasil.</p>
+                </article>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+                <a href="#contato"><button>Todos os Cases de Sucesso</button></a>
+            </div>
+        </div> 
+    </div>   
+</section> 
+    
+<div id="frase" class="esconder">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding wow fadeInDown">
+                <p>"Acreditamos na enorme vocação tecnológica da cidade de Campinas. Investimos e realizamos vários eventos quais fortalecem a capacitação e compartilhamento de experiências da comunidade de desenvolvimento de software".</p>
+            </div>    
+        </div>
+    </div>   
+</div> 
+    
+<section id="contato">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 padding">
+                <center><h1>Conta<p class="o">t</p>o. </h1>
+                <span>ADORARÍAMOS TE CONHECER. VAMOS TOMAR UM CAFÉ?</span></center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 wow bounceIn">
+                <img alt="" title="" border="0" src="img/endereco.png" />
+                <h2>Estamos em Campinas</h2>
+                <p>Complexo Pólis de Tecnologia</p>
+                <p>Rua Dr Ricardo Benetton Martins, 1000, Prédio 20 - Polo de Alta Tecnologia - Campinas/SP CEP 13086-902</p>
+            </div>
+            <div class="col-md-4 wow bounceIn" data-wow-delay=".2s">
+                <img alt="" title="" border="0" src="img/telefone.png" />
+                <h2>Vamos Conversar</h2>
+                <p>Campinas<br>+55 19 3256-6722</p>
+                <p>São Paulo<br>+55 11 3014-0305</p>
+            </div>
+            <div class="col-md-4 wow bounceIn" data-wow-delay=".3s">
+                <img alt="" title="" border="0" src="img/email.png" />
+                <h2>Escreva para Nós</h2>
+                <p>contato@</p>
+                <p>dextra.com.br</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 col-xs-4 wow flipInX">
+                <a href="https://www.facebook.com/dextrasis" target="_blank">
+                    <img alt="Facebook Dextra" title="Facebook Dextra" border="0" src="img/facebook.png" />
+                </a>                    
+            </div>
+            <div class="col-md-2 col-xs-4 wow flipInX" data-wow-delay=".2s">
+                <a href="https://www.facebook.com/dextrasis" target="_blank">
+                    <img alt="Instagram Dextra" title="Instagram Dextra" border="0" src="img/instagram.png" />
+                </a>                    
+            </div>
+            <div class="col-md-2 col-xs-4 wow flipInX" data-wow-delay=".3s">
+                <a href="https://www.linkedin.com/company/dextra-sistemas" target="_blank">
+                    <img alt="LinkedIn Dextra" title="LinkedIn Dextra" border="0" src="img/linkedin.png" />
+                </a>                    
+            </div>
+            <div class="col-md-2 col-xs-4 wow flipInX" data-wow-delay=".2s">
+                <a href="https://www.youtube.com/user/dextrasistemas" target="_blank">
+                    <img alt="Youtube Dextra" title="Youtube Dextra" border="0" src="img/youtube.png" />
+                </a>                    
+            </div>
+            <div class="col-md-2 col-xs-4 wow flipInX" data-wow-delay=".3s">
+                <a href="https://twitter.com/Dextra_digital" target="_blank">
+                    <img alt="Twitter Dextra" title="Twitter Dextra" border="0" src="img/twitter.png" />
+                </a>                    
+            </div>
+            <div class="col-md-2 col-xs-4 wow flipInX" data-wow-delay=".2s">
+                <a href="https://plus.google.com/+DextraBr" target="_blank">
+                    <img alt="Google Plus Dextra" title="Google Plus Dextra" border="0" src="img/googleplus.png" />
+                </a>                    
+            </div>
+        </div>
+        <div class="row">
+            <hr />
+            <div class="col-md-6">
+                <p>© Copyright - DEXTRA</p>
+            </div>
+            <div class="col-md-6">
+                <p>Desenvolvido por Matheus Decleve</p>
+            </div>
+        </div>
+    </div>
+</section>    
+    
+    
+
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js'></script>
+<script src="js/compressed.js"></script>
+
+  <script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+  </script>
+   
+</body>
+</html>
