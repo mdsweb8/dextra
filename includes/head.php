b6c8e23675c33544e017b03be5f4de031e931724
mdsweb8 <31604373+mdsweb8@users.noreply.github.com>
2017-09-03 23:17:01 -0300

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8"/>
+<title>Dextra - Desenvolvimento de Softwares</title>
+<link rel="stylesheet" href="css/animate.css" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/grids.css" />
+<meta name="viewport"  content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="img/favicon.png" type="image/png" />
+<meta property="og:url" content="http://www.dextra.com.br" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Dextra" />
+<meta property="og:description" content="Desenvolvemos softwares sob medida para negócios digitais." />
+
---
+++ includes/menu.php
@@ -0,0 +1,17 @@
+<main>
+    <nav>
+        <h1 class="none">Menu de Navegação</h1>
+        <div class="nav-cont">
+            <a id="logo" href="index.php"><img alt="Logo Dextra" title="Logo Dextra" border="0" src="img/dextra-logo.png" /></a>
+            <img src="img/menu.png" id="hamburger" />
+        </div>
+        <div id="list">
+            <a href="index.php">Home</a>
+            <a href="#dextra"> Sobre</a>
+            <a href="#solucoes"> Soluções</a>
+            <a href="#cases"> Cases</a>
+            <a href="https://jobs.kenoby.com/dextra/">Carreiras</a>
+            <a href="#contato"> Contato</a>
+        </div>
+    </nav>
+</main>
